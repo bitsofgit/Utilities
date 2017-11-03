c8c9e7fca88fffa86be97798fe236affcea379e8
Akhil Deshpande <deshpandeakhil@gmail.com>
2017-11-03 14:02:38 -0400

    additions

--- Utilities/HR/Algo_BitManipulation.cs
+++ Utilities/HR/Algo_BitManipulation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Utilities.HR
 {
@@ -11,7 +12,65 @@ namespace Utilities.HR
         public static void Test()
         {
             //LonelyInteger();
-            MaximizingXOR();
+            //MaximizingXOR();
+            CounterGame();
+        }
+
+        private static void CounterGame()
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            if (testCases < 1 || testCases > 10)
+                throw new Exception("Invalid argument");
+
+            long[] ns = new long[testCases];
+            for (int i = 0; i < testCases; i++)
+            {
+                long val = Convert.ToInt64(Console.ReadLine());
+                if (val < 1 || val > Math.Pow(2, 64) - 1)
+                    throw new Exception("Invalid argument");
+
+                ns[i] = val;
+            }
+
+            long[] vals = new long[64];
+            for(int i = 1; i <= 64; i++)
+            {
+                vals[i-1] = (long)Math.Pow(2, i);
+            }
+
+            foreach (var n in ns)
+            {
+                long tempn = n;
+                int counter = 1;
+                while (tempn != 1)
+                {
+                    counter++;
+                    if (IsPowerOf2(tempn))
+                    {
+                        tempn = tempn / 2;
+                    }
+                    else
+                    {
+                        tempn = tempn - HighestPowerof2(tempn, vals);
+                    }
+                }
+
+                if (counter % 2 == 0)
+                    Console.WriteLine("Louise");
+                else
+                    Console.WriteLine("Richard");
+            }
+        }
+
+        private static long HighestPowerof2(long tempn, long[] vals)
+        {
+            return vals.Where(n => n < tempn).Max();
+            
+        }
+
+        private static bool IsPowerOf2(long tempn)
+        {
+            return (tempn & (tempn - 1)) == 0;
         }
 
         private static void MaximizingXOR()
